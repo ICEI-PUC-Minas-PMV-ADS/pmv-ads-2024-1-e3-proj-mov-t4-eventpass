35baba113a45aea4e929076b12cafc2b54adbc44
Julia Chaves Dutra <jcdutra@sga.pucminas.br>
2024-04-23 22:11:04 -0300

    Atualiza react e api

--- src/EventPass/View/src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const api = axios.create({
-  baseURL: 'https://localhost:8080/',
-})
-
-export default api
---
+++ src/IMAGE-SERVER.md
@@ -0,0 +1,2 @@
+cd src/EventPass/assets/flyer
+pushd .; python3 -m http.server 9999; popd;
