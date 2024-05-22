bbe3a8974bd55e092131db407d1fdd10d4e2a44a
Julia Chaves Dutra <jcdutra@sga.pucminas.br>
2024-05-21 22:25:05 -0300

    Adiciona endpoint de buscar evento

--- eventpass-api/Services/EventosService.cs
+++ eventpass-api/Services/EventosService.cs
@@ -34,6 +34,21 @@ namespace EventPass.Services
             return eventos.ToList();
         }
 
+        public List<Evento> BuscarEventos(string query)
+        {
+            var eventos = appDbContext.Eventos
+                .Where(e => e.NomeEvento.ToUpper().Contains(query.ToUpper()) || e.Descricao.ToUpper().Contains(query.ToUpper()))
+                .OrderByDescending(e => e.DataHora)
+                .Take(10);
+            
+            foreach (Evento? evento in eventos)
+            {
+                evento.Flyer = storageService.GetFileImageStorageUrl(evento.Flyer);
+            }
+
+            return eventos.ToList();
+        }
+
         public Evento? FindById(int id)
         {
             Evento? evento = appDbContext.Eventos.Find(id);
