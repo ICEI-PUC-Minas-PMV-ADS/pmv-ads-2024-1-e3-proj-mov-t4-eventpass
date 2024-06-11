96789cbc1ddafc73e20a294aefb8c57cccdfe038
Sandro Shayne dutra <39307787+SandroShayne@users.noreply.github.com>
2024-06-11 15:31:16 -0300

    Corrigido

--- eventpass-back/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EventPass.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- eventpass-back/Models/EventoIngressoViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EventPass.Models
-{
-    public class EventoIngressoViewModel
-    {
-        public Evento Evento { get; set; }
-        public Ingresso Ingresso { get; set; }
-    }
-}
